313a1ad8c127aecc67d174fc514728d4cd13f8e6
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2021-05-05 23:40:16 -0600

    Update preprocess.py

--- audio_visual/preprocess.py
+++ audio_visual/preprocess.py
@@ -48,11 +48,8 @@ def main():
     print("\n\nStarting preprocessing ....\n")
 
     params = {"roiSize":args["ROI_SIZE"], "normMean":args["NORMALIZATION_MEAN"], "normStd":args["NORMALIZATION_STD"], "vf":vf}
-    for file in tqdm(filesList[0:50000], leave=True, desc="Preprocess", ncols=75):
+    for file in tqdm(filesList, leave=True, desc="Preprocess", ncols=75):
         print(file)
-        preprocess_sample(file, params)
-
-    for file in tqdm(filesList[49999:], leave=True, desc="Preprocess", ncols=75):
         preprocess_sample(file, params)
 
     print("\nPreprocessing Done.")
